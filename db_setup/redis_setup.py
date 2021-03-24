be784da99ef48984183274d6ae49853f32a83432
sansqra <sqrasan@gmail.com>
2021-03-24 20:49:31 +0530

    redis ref in redis_setup

--- db_setup/redis_setup.py
+++ db_setup/redis_setup.py
@@ -4,4 +4,26 @@ import os
 
 load_dotenv()
 
+
+'''
+        LISTEN UP
+
+        # default = 86400
+
+    # t = 3
+    r = redis.Redis(blah blah blah)
+
+    # r.set('uid', 'b2hj430', ex=t)
+
+    # print(r.get("uid"))
+
+    # time.sleep(2)
+    r.flushall()
+    # r.delete("uid")
+    print(r.get("uid"))
+    # print(r.ttl("uid"))
+
+
+'''
+
 REDIS_CONN = redis.Redis(host=os.getenv("REDIS_URL"), password=os.getenv("REDIS_PORT"), port=os.getenv("REDIS_PORT"))
