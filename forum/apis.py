d84a9d3d6276147f160525d238a33af3c700ea32
Keane Pereira <keane3pereira@gmail.com>
2021-03-26 14:00:43 +0530

    )

--- forum/apis.py
+++ forum/apis.py
@@ -15,5 +15,5 @@ async def get_forum_chat():
     pass
 
 @router.post('/send_message/')
-async def send_message(classroom_uid: str = Body(...), message: str = Body(...), token: dict = Depends(token_validation):
+async def send_message(classroom_uid: str = Body(...), message: str = Body(...), token: dict = Depends(token_validation)):
     return await forum_routes.send_message(classroom_uid=classroom_uid, message=message, token=token)
