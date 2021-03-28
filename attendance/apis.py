4afccc98ea626a4986f03435b79b78c9b2606bdb
Keane Pereira <keane3pereira@gmail.com>
2021-03-28 17:41:33 +0530

    api separation

--- attendance/apis.py
+++ attendance/apis.py
@@ -12,7 +12,7 @@ ATTENDANCE_DEFAULT_TOKEN_TIMEOUT = 30 * 60
 
 router = APIRouter()
 
-@router.post('/take_attendance/')
+@router.post('/take_attendance/', tags = ["attendance_teacher"])
 async def take_attendance(classroom_uid: str = Body(...), timeout_minutes: Optional[int] = Body(30), token: dict = Depends(token_validation)):
     if timeout_minutes == "" or not timeout_minutes:
         timeout_minutes = ATTENDANCE_DEFAULT_TOKEN_TIMEOUT
@@ -21,28 +21,28 @@ async def take_attendance(classroom_uid: str = Body(...), timeout_minutes: Optio
     return await attendance_routes.take_attendance(token = token, classroom_uid=classroom_uid, timeout = timeout_minutes)
 
 
-@router.post('/stop_attendance/')
+@router.post('/stop_attendance/', tags = ["attendance_teacher"])
 async def stop_attendance(classroom_uid: str = Body(...), attendance_token: str = Body(...), token: dict = Depends(token_validation)):
     return await attendance_routes.stop_attendance(token=token, classroom_uid=classroom_uid, attendance_token=attendance_token)
 
 
-@router.post('/delete_attendance/')
+@router.post('/delete_attendance/', tags = ["attendance_teacher"])
 async def delete_attendance(classroom_uid: attendance_schemas.TakeAttendance, attendance_token: str, token: dict = Depends(token_validation)):
     return await attendance_routes.delete_attendance(token=token, classroom_uid=classroom_uid.classroom_uid, attendance_token=attendance_token)
 
 
-@router.post("/view_classroom_attendance/")
+@router.post("/view_classroom_attendance/", tags = ["attendance_teacher"])
 async def view_classroom_attendance(classroom_uid: attendance_schemas.TakeAttendance, token: dict = Depends(token_validation)):
     return await attendance_routes.view_classroom_attendance(token = token, classroom_uid = classroom_uid)
 
 ''' Student APIs '''
 
-@router.post('/give_attendance/')
+@router.post('/give_attendance/', tags = ["attendance_student"])
 async def give_attendance(classroom_uid: str = Body(...), attendance_token: str = Body(...), token: dict = Depends(token_validation)):
     return await attendance_routes.give_attendance(token=token, classroom_uid=classroom_uid, attendance_token=attendance_token)
 
 ''' Common APIs '''
 
-@router.post("/view_student_attendance")
+@router.post("/view_student_attendance", tags = ["attendance_teacher", "attendance_student"])
 async def view_student_attendance(classroom_uid: attendance_schemas.TakeAttendance, token: dict = Depends(token_validation)):
     return await attendance_routes.view_student_attendance(token = token, classroom_uid = classroom_uid)
