a44c37f6bd5007ecaa8900e770ad1c64c49d4dd6
sansqra <sqrasan@gmail.com>
2021-04-04 11:39:06 +0100

    minor fixes

--- ztest.py
+++ ztest.py
@@ -8,7 +8,17 @@ load_dotenv()
 x = datetime.datetime.now()
 print(x)
 
-localized = timezone("Asia/Kolkata")
-print(localized.localize(x))
+# localized = timezone("Asia/Kolkata")
+# print(localized.localize(x))
 
-print(x.astimezone(timezone("Asia/Kolkata")))
+# print(x.astimezone(timezone("Asia/Kolkata")))
+
+
+# from dropbox_setup import DBX
+# from dotenv import load_dotenv
+# load_dotenv()
+
+
+# x = DBX.sharing_create_shared_link_with_settings("/profile_pictures/")
+
+# print(x)
