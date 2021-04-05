e4bfcb01cb99f672afb6ee8d8321f0c1040d4ef9
Keane Pereira <keane3pereira@gmail.com>
2021-04-05 13:45:04 +0100

    upload file

--- storage/routes.py
+++ storage/routes.py
@@ -15,23 +15,49 @@ async def create_folder(classroom_uid, folder_name, path, token):
             return StandardResponseBody(
                 False, "Folder already exists", token.token_value
             )
+        return StandardResponseBody(
+            True, "Folder could not be created", token.token_value
+        )
     return StandardResponseBody(
-        False, "Folder not created"
+        False, "User not authorized"
     )
 
+
+async def upload_file(token, classroom_uid, path, file):
+    res = await classroom_controllers.check_user_if_creator(classroom_uid, token.user_id)
+    if res:
+        res2 = await storage_controllers.upload_file(path, file)
+        if res2:
+            return StandardResponseBody(
+                True, "File uploaded", token.token_value
+            )
+        return StandardResponseBody(
+            True, "File could not be uploaded", token.token_value
+        )
+    return StandardResponseBody(
+        False, "User not authorized."
+    )
 
 
 async def get_files_and_folders(classroom_uid, path, token):
     res1 = await classroom_controllers.check_user_if_creator(classroom_uid, token.user_id)
     res2 = await classroom_controllers.if_user_enrolled(classroom_uid, token.user_id)
-    if res1 or res2:
-        res = await storage_controllers.get_files_and_folders(path)
-        if res:
+    if classroom_uid in path:
+        if res1 or res2:
+            res = await storage_controllers.get_files_and_folders(path)
+            if res:
+                return StandardResponseBody(
+                    True, "Files retrieved", token.token_value, res
+                )
+            elif res == []:
+                return StandardResponseBody(
+                    True, "Files retrieved", token.token_value, []
+                )
             return StandardResponseBody(
-                True, "Files retrieved", token.token_value, res
+                True, "Files could not be retrieved", token.token_value
             )
     return StandardResponseBody(
-        False, "Files not retrieved"
+        False, "User not authorized"
     )
 
 
@@ -45,7 +71,7 @@ async def get_file_download_link(classroom_uid, path, token):
             False, "Link retrieved", res
         )
     return StandardResponseBody(
-        False, "Link not retrieved"
+        False, "User not authorized"
     )
 
 
@@ -57,6 +83,9 @@ async def delete_file(classroom_uid, path, token):
             return StandardResponseBody(
                 True, "File / folder deleted", token.token_value
             )
+        return StandardResponseBody(
+            True, "File / folder could not be deleted", token.token_value
+        )
     return StandardResponseBody(
-        False, "File / folder not deleted"
+        False, "User not authorized"
     )
