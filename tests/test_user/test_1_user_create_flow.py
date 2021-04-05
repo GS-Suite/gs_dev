4209d4e49967fadb8cb2f46cc4db9af83408795c
Keane Pereira <keane3pereira@gmail.com>
2021-04-05 07:46:44 +0100

    user basic tests

--- tests/test_user/test_1_user_create_flow.py
+++ tests/test_user/test_1_user_create_flow.py
@@ -42,29 +42,3 @@ def test_sign_up_existing():
 
     res = json.loads(response._content)
     assert res["success"] == False
-
-
-def test_sign_in():
-    global TOKEN
-    response = requests.post(
-        url = f"{BASE_URL}/sign_in/",
-        json = {"username": "test_username", "password": "test_password"}
-    )
-    assert response.status_code == 200
-    
-    res = json.loads(response._content)
-    assert res["success"] == True
-    TOKEN = res["token"]
-
-
-def test_sign_out():
-    global TOKEN
-    response = requests.post(
-        url = f"{BASE_URL}/sign_out/",
-        headers = {"token": TOKEN}
-    )
-    assert response.status_code == 200
-    
-    res = json.loads(response._content)
-    assert res["success"] == True
-    TOKEN = None
---
+++ tests/test_user/test_3_user_delete.py
@@ -0,0 +1,48 @@
+import requests
+import json
+
+from dotenv import load_dotenv
+load_dotenv()
+import os
+
+
+BASE_URL = os.getenv("BASE_URL")
+
+TOKEN = None
+
+def test_sign_in():
+    global TOKEN
+    response = requests.post(
+        url = f"{BASE_URL}/sign_in/",
+        json = {"username": "test_username", "password": "test_password"}
+    )
+    assert response.status_code == 200
+    
+    res = json.loads(response._content)
+    assert res["success"] == True
+    TOKEN = res["token"]
+
+def test_delete_account():
+    global TOKEN
+    response = requests.post(
+        url = f"{BASE_URL}/delete_account/",
+        json = {"password": "test_password"},
+        headers = {"token": TOKEN}
+    )
+    assert response.status_code == 200
+    
+    res = json.loads(response._content)
+    assert res["success"] == True
+    TOKEN = None
+
+
+def test_sign_in_again():
+    global TOKEN
+    response = requests.post(
+        url = f"{BASE_URL}/sign_in/",
+        json = {"username": "test_username", "password": "test_password"}
+    )
+    assert response.status_code == 200
+    
+    res = json.loads(response._content)
+    assert res["success"] == False
